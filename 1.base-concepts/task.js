6647937ecd9b44e06b40316730bb4c819446b9c9
Evgeny Fedotov <riba-baskervili@ya.ru>
2024-07-28 22:44:42 +0300

    BJS-72. Основные понятия. Решение домашнего задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+
+  let d = b**2 - 4 * a * c;
+
+  if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+  } else if (d > 0) {
+    let firstRoot = (-b + Math.sqrt(d) )/(2*a);
+    let secondRoot = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(firstRoot, secondRoot);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let TotalMortgage = monthlyPayment * countMonths;
+
+  return +TotalMortgage.toFixed(2);
 }
